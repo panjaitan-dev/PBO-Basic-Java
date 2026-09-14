c91d8d7f44f58e5a081cea141a06220e74446eb8
panjaitan-dev <249464640+panjaitan-dev@users.noreply.github.com>
2026-09-14 20:06:34 +0700

    tambah

--- Demo.java
+++ Demo.java
@@ -1,6 +1,31 @@
+/* Penamaan Variabel 
+- Case-sensitive (besar kecil huruf dari nama variabel → berbeda).
+- Nama harus mulai dengan huruf (tidak boleh mulai dengan angka atau simbol).
+- Hindari $ dan _, kecuali dalam kasus tertentu.
+- Tidak boleh pakai keyword Java (class, int, if, dll.).
+- Gunakan nama yang jelas & mudah dipahami (totalNilai, bukan tn).
+- Gunakan final untuk konstanta.
+    Konvensi: Contoh:
+    Variabel → camelCase (totalGaji, rataNilai).
+    Konstanta → UPPER_CASE dengan _ (MAX_SIZE, PI).
+*/
+contoh :int umur = 20;
+        double gaji = 3500000.50
+        boolean lulus = true;
+        String nama = "Budi";
 
+// Class adalah cetak biru (blueprint) untuk membuat objek
+// Di dalamnya terdapat variabel(yang disebut atribut/field) dan fungsi yang disebut method        
+// Tipe data class termasuk tipe data non-primitive (yang menyimpan alamat/memori)
 class Demo {
     int a, b;
     Demo(int a, int b) { this.a = a; this.b = b; }
     int addition() { return a + b; }
 }
+// String adalah objek khusus untuk teks (bukan primitive).
+ String s1 = "Hello";
+ String s2 = new String("Java");
+
+ // Array adalah koleksi elemen bertipe sama , diakses via indeks
+ int[] arr = {1, 2, 3};
+ double[] arr2 = {1.1, 2.2};
--- HelloWorld.java
+++ HelloWorld.java
@@ -19,3 +19,28 @@ class sebagai kelas yang akan dijalankan,
 public static void main(String[] args) sebagai pintu masuk program,
 System.out.println sebagai cara untuk mencetak ke konsol.
 */
+
+/*
+Variabel: Tempat penyimpanan nilai atau data di dalam memori komputer saat program berjalan. 
+Tipe Data: Jenis nilai yang disimpannya. Terdiri dari tipe data primitive untuk penanganan nilai langsung (seperti int, double, boolean, char) dan non-primitive untuk data terstruktur/objek (seperti String, Array, Class, Interface)
+
+Jenis Variabel
+Dalam Java, istilah "field" sebagai object dan "variabel" sama-sama digunakan dan sering kali merujuk pada hal yang sama, sehingga membingungkan para pengembang baru. Java mendefinisikan jenis-jenis variabel berikut:
+1.Instance Variables (Non-Static Fields) 
+  - Milik setiap objek (unik di setiap instance). */
+   contoh: int speed; -> tiap objek punya nilai sendiri.
+2.Class Variables  (Static Fields)
+    -Hanya 1 salinan untuk semua objek dari class tsb
+    -Dideklarasikan dengan static */
+    -contoh: static int jumlahMahasiswa;
+3.Local Variables
+    -Dideklarasikan didalam method
+    -Hanya bisa diakses dalam method itu aja  */
+    -contoh: void hitung() {
+    int count = 0; // local variable
+    }
+4.Parameters
+    -Variabel input untuk method/constructor  */
+    -contoh:void setNama(String nama) {
+    this.nama = nama;
+}
